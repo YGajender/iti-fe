e25ccf95302a33e828a0128f85420fd182aaa23f
YGajender <gajenderdahina@gmail.com>
2025-02-28 02:22:17 +0530

    28-02-2015

--- src/components/VideoPlayer/Videoplayer.jsx
+++ src/components/VideoPlayer/Videoplayer.jsx
@@ -1,16 +1,18 @@
 import { useRef } from "react";
-import video from "../../assets/college-video.mp4";
 import "./Videoplayer.css";
+
 const Videoplayer = ({ playState, setPlayState }) => {
   const player = useRef(null);
+
   const closePlayer = (e) => {
-    if (e.target === player.current) {
+    if (e.target === e.currentTarget) {
       setPlayState(false);
     }
   };
+
   return (
     <div
-      className={`video-player ${playState ? "" : "hide"}`}
+      className={`video-player ${!playState ? "hide" : ""}`}
       ref={player}
       onClick={closePlayer}
     >
